438df0ec1780157fb8eade8f3b3015923da38da1
zhudongya <dongya.zhu@maxjia.com>
2022-08-24 14:44:05 +0800

    add code

--- 04/main.go
+++ 04/main.go
@@ -2,31 +2,47 @@ package main
 
 import (
 	"context"
-	v1 "k8s.io/api/core/v1"
-	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	//config
+	////config
+	//config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//config.GroupVersion = &v1.SchemeGroupVersion
+	//config.NegotiatedSerializer = scheme.Codecs
+	//config.APIPath = "/api"
+	//
+	////client
+	//client, err := rest.RESTClientFor(config)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	////get
+	//pod := v1.Pod{}
+	//err = client.Get().Namespace("default").Resource("pods").Name("test").Do(context.TODO()).Into(&pod)
+	//if err != nil {
+	//	println(err)
+	//} else {
+	//	println(pod.Namespace)
+	//	println(pod.Name)
+	//}
+
 	config, err := clientcmd.BuildConfigFromFlags("", clientcmd.RecommendedHomeFile)
 	if err != nil {
 		panic(err)
 	}
-	config.GroupVersion = &v1.SchemeGroupVersion
-	config.NegotiatedSerializer = scheme.Codecs
-	config.APIPath = "/api"
-
-	//client
-	client, err := rest.RESTClientFor(config)
+	clientSet, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err)
 	}
-
-	//get
-	pod := v1.Pod{}
-	err = client.Get().Namespace("default").Name("test").Do(context.TODO()).Into(&pod)
+	coreV1 := clientSet.CoreV1()
+	pod, err := coreV1.Pods("default").Get(context.TODO(), "test", v1.GetOptions{})
 	if err != nil {
 		println(err)
 	} else {
